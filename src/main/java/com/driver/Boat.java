30da621b282948e826bd0bd7f8ad4b77ce350fd1
prince3017 <princemahtojspr@gmail.com>
2023-01-21 16:23:03 +0530

    First

---
+++ src/main/java/com/driver/Boat.java
@@ -0,0 +1,33 @@
+package com.driver;
+
+public  class Boat implements WaterVehicle{
+    String name;
+    int capacity;
+
+    public Boat(String name, int capacity){
+        this.name = name;
+        this.capacity = capacity;
+    }
+
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    @Override
+    public String getVehicleName() {
+        return null;
+    }
+
+    @Override
+    public int getVehicleCapacity() {
+        return 0;
+    }
+}
--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -4,6 +4,7 @@ public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
         //Use arbitrary values for parameters which are not mentioned
+        super(name , 4,4,6,isManual,name,4);
     }
 
     public void accelerate(int rate){
@@ -20,7 +21,20 @@ public class F1 extends Car {
 
         if(newSpeed == 0) {
             //Stop the car, set gear as 1
-        }
+            changeGear(1);
+
+        }else if(newSpeed>=1 && newSpeed<=50)
+            changeGear(1);
+        else if(newSpeed>50 && newSpeed<=100)
+            changeGear(2);
+        else if(newSpeed>100 && newSpeed<=150)
+            changeGear(3);
+        else if(newSpeed>150 && newSpeed<=200)
+            changeGear(4);
+        else if(newSpeed>200 && newSpeed<=250)
+            changeGear(5);
+        else if(newSpeed> 250)
+            changeGear(6);
         //for all other cases, change the gear accordingly
 
         if(newSpeed > 0) {
