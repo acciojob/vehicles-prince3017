2fb8bd1e10de2211031a2962e654b66f108eacc0
prince3017 <princemahtojspr@gmail.com>
2023-01-21 17:46:10 +0530

    hii

--- src/main/java/com/driver/Vehicle.java
+++ src/main/java/com/driver/Vehicle.java
@@ -17,7 +17,7 @@ public class Vehicle {
 
     public void steer(int direction){
         //direction is in degrees, add it to the current direction
-        this.currentDirection += direction;
+        this.currentDirection = direction + currentDirection;
         System.out.println("steer method called - The direction is changed to: " + currentDirection + " degrees");
     }
 
@@ -43,7 +43,9 @@ public class Vehicle {
         return currentSpeed;
     }
 
-
+    public int currentDirection(){
+        return currentDirection;
+    }
 
     public String getName() {
         return name;
